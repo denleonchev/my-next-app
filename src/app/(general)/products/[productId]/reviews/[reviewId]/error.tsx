da2bbfe2d31d194b73ead74798ad2e6b71d8af06
denleonchev <den.leonchev@gmail.com>
2025-09-27 16:39:03 +0000

    Add resetting after error

--- src/app/(general)/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/(general)/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,8 +1,22 @@
 "use client";
 
-export default function ReviewDetailsError() {
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+
+export default function ReviewDetailsError({ error, reset }: { error: Error; reset: () => void }) {
+  const router = useRouter();
+  const [, startTransition] = useTransition();
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  }
   return (
-    <h1>Error in reviewId</h1>
+    <>
+      <h1>Error {error.message}</h1>
+      <button onClick={reload}>Try again</button>
+    </>
   );
 }
              
